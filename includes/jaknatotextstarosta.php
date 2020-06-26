cabda37ff8ee9dfe3f3f0e6557ac20a00cdb9a5e
SimonB87 <buryans@gmail.com>
2020-06-26 22:27:11 +0200

    app status June 2020

---
+++ includes/jaknatotextstarosta.php
@@ -0,0 +1,106 @@
+<h2 class="major"> <span style="color: var(--theme-color);">Pro starosty:</span> Jak na to <br>(od námětu k realizaci) </h2>
+<p></p>
+<h4 class="major"> Aplikace námětu do prostředí obce </h4>
+<p></p>
+
+<p>Pro aplikaci námětu je nutné zhodnotit dostupné kapacity:</p>
+
+<ul>
+    <li>Personální: koho se námět dotýká, kdo je ochoten se zapojit do řešení</li>
+    <li>Ekonomické: jaké jsou investiční a provozní nároky námětu</li>
+    <li>Infrastrukturní: jaké jsou nároky na realizaci a provoz námětu z hlediska prostoru, logistické dostupnosti, servisu </li>
+</ul>
+
+<p></p>
+
+<p>Dále je potřeba posoudit, zda je námět pro obec jako takovou vhodný (z pohledu prostorových dispozic, velikosti obce, vstupů, 
+využitelnosti případných výstupů, reálnosti navázání spolupráce s potřebnými partnery atd.)</p>
+
+<p>Jako starosta nebo zastupitel vycházím z konkrétních potřeb a možností obce a z předpokládané ochoty občanů k zapojení se 
+do realizace budoucího záměru.</p>
+
+<ul>
+    <li>Vlastní obecní řešení: k realizaci námětu není potřeba zapojení občanů ani prostředků mimo obecní rozpočet.</li>
+    <li>Obecní řešení s příspěvkem: k realizaci záměru je třeba prostředků mimo obecní rozpočet – z veřejných prostředků nebo 
+    od místních podnikatelů.</li>
+    <li>Obecní řešení se zapojením občanů: k realizaci a provozu záměru je třeba zapojit a motivovat občany.</li>
+    <li>Regionální řešení: se zapojením dalších obcí či mikroregionu.</li>
+    <li>Integrované řešení: vyžadující spolupráci více typů subjektů na různých úrovních.</li>    
+</ul>
+
+<p></p>
+
+<h4 class="major">Příprava konkrétního záměru</h4>
+
+<p>Věnujte se pozorně těmto krokům:</p>
+
+<ul>
+    <li>Definice záměru a cíle: proč je vhodné záměr realizovat, čemu přispěje. Jaký je jeho cíl a je cíl měřitelný (např. snížením produkce 
+    nevyužitelného odpadu)? Cíle záměru je vhodné stanovit tak, aby bylo měřitelné jejich dosažení.</li>
+    <li>Mezi popisem stávajícího a zamýšleného stavu musí být zřejmý pozitivní rozdíl.</li>
+    <li>Definice cílové skupiny a dotčených subjektů: pro koho je námět určen, kdo se na něm může nebo musí podílet, koho se dotkne. 
+    Pro koho bude mít pozitivní a případné negativní dopady. Vhodné je definovat skupiny v co nejširším měřítku, 
+    zejména v případě potenciálně negativně dotčených subjektů, včetně návrhu eliminačních nebo kompenzačních opatření.</li>
+    <li>Definice potřebných ekonomických prostředků: jaké jsou realizační a provozní náklady záměru, kde je možné tyto prostředky získat. 
+    Tento krok zahrnuje zhodnocení vlastních možností (občana, komunity, obce, podnikatele) a případné hledání dalších možností 
+    financování – veřejné podpory, veřejné lokální sbírky apod.</li>
+    <li>Definice potřeb z hlediska infrastruktury: jaký prostor a infrastruktura jsou nezbytné k realizaci. Je možné záměr realizovat 
+    kdekoliv, na několika nebo pouze na jednom konkrétním místě. Jaké místo je ideální a kdo je jeho vlastníkem. Kdo vlastní a je ochoten 
+    poskytnout místo nebo infrastrukturu a za jakých podmínek. </li>
+</ul>
+
+<p>Vhodné je zpracování alespoň základní SWOT analýzy projektu, která obsahuje vyhodnocení silných a slabých stránek záměru a z něho 
+vyplývajících příležitostí a hrozeb.</p>
+
+<p>Velkým přínosem může být nalezení podobných příkladů (realizací) a komunikace s jejich realizátory. Tento krok pomůže s optimalizací záměru, 
+včetně včasného snížení rizik.</p>
+
+<p></p>
+
+<h4 class="major">Hodnocení záměru a rozhodnutí o něm</h4>
+
+<p>Záměr je potřeba zhodnotit z několika aspektů:</p>
+
+<ul>
+    <li>environmentální dopady: jaké bude mít realizace záměru důsledky z hlediska zvyšování využití odpadů, snižování emitované dopravy apod., 
+    a především z hlediska možných negativních vlivů – emisí škodlivin, hluku, prachu, zápachu nebo vlivu na estetický ráz.</li>
+    <li>ekonomické dopady – jaký bude mít realizace a provoz záměru vliv na rozpočet komunity / obce / místní organizace. Jsou tyto náklady 
+    účelné, nebo je možné je využít efektivněji?</li>
+    <li>dlouhodobá udržitelnost – co je nezbytné k dlouhodobému zajištění záměru z hlediska všech výše uvedených aspektů – personální 
+    a ekonomické náročnosti, environmentálních dopadů atd.</li>
+</ul>
+
+<p>Starosta či zastupitel má větší možnosti a zároveň povinnosti analýzy záměru z hlediska možností obce (rozpočtu, zaměstnanců, 
+hmotného majetku). V případě, že uznám projekt za realizovatelný, zpracuji jej do vhodné zprávy, kterou přednesu zastupitelstvu. 
+Předtím jej mohu konzultovat s externími experty, zainteresovanými zastupiteli nebo zástupci místních, do problematiky zapojených institucí. 
+Zastupitelstvo následně rozhodne o realizaci projektu, případně jeho podrobnější přípravě (zpracování studie proveditelnosti apod.).</p>
+
+<p>Podrobný popis postupu je uveden <span style="color:red;">v kapitole metodiky 2.4 <span>.</p>
+
+<p></p>
+
+<h4 class="major">Realizace záměru a zapojení dalších subjektů</h4>
+
+<p>V některých případech je vhodné rozčlenit záměr do několika etap z hlediska kapacity a rozsahu, a to z důvodu snížení náročnosti realizace, 
+ale i získávání průběžné zpětné vazby.</p>
+
+<p>Během realizace je nutné zejména přehledné a efektivní řízení projektu, sledování souladu jeho realizace s projektovou dokumentací 
+a plnění dílčích cílů.</p>
+
+<p>V případě realizace projektu s veřejnou podporou je třeba dodržet parametry obdržené podpory, vyplývající z rozhodnutí/smlouvě 
+o dotaci/grantu, a to se zvláštním důrazem na veřejné zakázky a jejich zadávání.</p>
+
+<p>Obecně lze konstatovat, že během realizace je zapotřebí dohlížet na plnění projektové dokumentace, harmonogramu a rozpočtu projektu, 
+bezpečnost práce, profesionalitu řešení, environmentální a další aspekty. Je důležité zajistit tento monitoring zodpovědnou 
+a způsobilou osobou.</p>
+
+<p></p>
+
+<h4 class="major">Monitoring a údržba realizovaného záměru</h4>
+
+<p>Jde o sledování stavu záměru z hlediska samotné existence, plánovaných dopadů, parametrů a výsledků, v návaznosti na definované cíle, 
+včetně vyhodnocování zpětné vazby. V rámci monitoringu je důležité zejména dohlížet na dlouhodobou udržitelnost a ekonomickou 
+i environmentální efektivitu a v případě potřeby činit nutná opatření (údržba, změna provozu, upgrade dle aktuálních podmínek apod.). 
+Pro monitoring je vhodné dlouhodobě stanovit zodpovědnou osobu.</p>
+
+<a href="index.php" class="button primary small">Zpátky domů</a>
