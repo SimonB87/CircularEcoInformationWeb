787a89ab43ee700e471b5470dc74ef5d22adf6c4
SimonB87 <buryans@gmail.com>
2020-06-28 11:25:44 +0200

    js of waste calc management

--- assets/js/wasteCalcManagement.js
+++ assets/js/wasteCalcManagement.js
@@ -1,12 +1,33 @@
+console.log("top place - doc state " + document.readyState);
+
+//"loading", "interactive", "complete"
+if (document.readyState == "complete") {
+  console.log("inside function - doc state " + document.readyState);
+
+  const targetElchilds = document.querySelector(".formToglerControls__itemNumbers").children;
+  for (var i = 0; i < targetElchilds.length; i++) {
+    targetElchilds[i].onclick = changeActiveChild(i);
+  }
+}
+
+if (document.readyState == "loading") {
+  console.log("inside function - doc state " + document.readyState);
+}
+
+if (document.readyState == "interactive") {
+  console.log("inside function - doc state " + document.readyState);
+}
+
+/*
 const targetElchilds = document.querySelector(".formToglerControls__itemNumbers").children;
 for (var i = 0; i < targetElchilds.length; i++) {
   targetElchilds[i].onclick = changeActiveChild(i);
 }
-
+*/
 
 function changeActiveChild(argument) {
-
-  console.log("active function " + argument);
+  //test
+  console.log("active function " + argument);//test
 
   let _this = this;
   let targets = document.querySelectorAll(".formToglerControls__itemNumber");
