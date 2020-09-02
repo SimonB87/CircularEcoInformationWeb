566a969acc354cd1dfad71eb4f0da1ddb5a828ad
SimonB87 <buryans@gmail.com>
2020-09-02 21:24:12 +0200

    projectDetaiPhp - added header and footer code

--- 3demo_testtabulka/projectDetailPdf.php
+++ 3demo_testtabulka/projectDetailPdf.php
@@ -5,6 +5,36 @@
 require("includes/fpdf182/fpdf.php");
 $pdf = new FPDF();
 
+//set PDF header and footer
+
+class PDF extends FPDF
+{
+// Page header
+function Header()
+{
+    // Logo
+    $this->Image('includes/fpdf182/logoPrint.png',10,6,30);
+    // Arial bold 15
+    $this->SetFont('Arial','B',15);
+    // Move to the right
+    $this->Cell(80);
+    // Title
+    $this->Cell(30,10,'Title',1,0,'C');
+    // Line break
+    $this->Ln(20);
+}
+
+// Page footer
+function Footer()
+{
+    // Position at 1.5 cm from bottom
+    $this->SetY(-15);
+    // Arial italic 8
+    $this->SetFont('Arial','I',8);
+    // Page number
+    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+}
+}
 
 
 require 'config/config2.php';
