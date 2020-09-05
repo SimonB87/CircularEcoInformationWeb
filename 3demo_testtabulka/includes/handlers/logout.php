4d334184ca3e6840df9055cabf63f571c10a94a9
SimonB87 <buryans@gmail.com>
2020-09-05 15:05:40 +0200

    renamed folder for sollutions - delete

--- 3demo_testtabulka/includes/handlers/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-  session_start();//start session.
-  session_destroy();//destroy session to log out.
-  header("Location: ../../register.php")//move user to the register page to relog-in.
- ?>
