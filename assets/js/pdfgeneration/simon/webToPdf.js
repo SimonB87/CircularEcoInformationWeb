b03f360a9f89f44cedd26a38eda725cc702fc3b7
SimonB87 <buryans@gmail.com>
2020-09-13 11:57:55 +0200

    Waste Calculator - added script for checking used charts array

--- assets/js/pdfgeneration/simon/webToPdf.js
+++ assets/js/pdfgeneration/simon/webToPdf.js
@@ -18,10 +18,16 @@ function getPDFFileButton() {
   
   const townName = document.getElementById("townName").innerHTML
 
-  const docHeader = `Obcevkruhu.cz - Analýza Odpadového hospodářství obce ${townName}`;
+  const docHeader = `Obcevkruhu.cz - Analýza Odpadového hospodářství pro obec: ${townName}`;
   const displayDateTime = dateTime;
 
   const fileName = "AnalyzaOH_" + document.getElementById("townName").innerText + ".pdf";
+
+  const usedChartsNumbers = getNumberOfUsedCharts();
+  const arrayOfPairs = splitArrayIntoArrayOfPairs(usedChartsNumbers);
+  //test
+  console.log(usedChartsNumbers);//test
+  console.log(arrayOfPairs);//test
 
   // here printing div with content ".content-body"
   // using html canvas to save as required pdf to image to preserve css
@@ -37,21 +43,64 @@ function getPDFFileButton() {
           var pdf = new jsPDF('p', 'mm', 'a4');
           pdf.setFont("FreeSerif");
           pdf.setFontSize(14);
-          pdf.text(docHeader, 20, 25);
-          pdf.text(displayDateTime, 20, 35);
+          pdf.text(docHeader, 15, 25);
+          pdf.text(displayDateTime, 15, 35);
           pdf.addImage(myImage, 'JPEG', 15, 45, imgWidth, imgHeight); // 2: 19
 
           pdf.save(fileName);
         }
     });
 
+  /* Support functions */
   function prepareWebForConversion() {
+
     drawChartColumns("pdfPrint");
     displayChartType("gauges","bars"); 
     document.querySelector("#results-content-body").classList.add("pdfPrint");
     document.querySelector("#results-content-body").classList.remove("mobileWidth");
     document.getElementById("resetViewfterPdfConversion").classList.remove("displayNone");
     document.getElementById("resetViewfterPdfConversion").classList.add("visible");
+
+  }
+
+  function getNumberOfUsedCharts(){
+
+    let outputArray = [];
+    for ( var i = 1; i < 16; i++ ) {
+  
+      let selector = "gaugeData" + i;
+  
+      if (! (window[selector]["x02_gaugeMainValue"] == 0)) {
+        outputArray.push(i);
+      }
+  
+    }
+    return outputArray;
+  }
+  
+  function splitArrayIntoArrayOfPairs(inputArray) {
+
+    let outputArray = [];
+  
+    for(var i = 0; i< inputArray.length; i = i + 2){
+      let row = [];
+      let firstItem;
+      let secondItem;
+      let pairArray = [];
+      if ( i < inputArray.length - 2 ) {
+        firstItem = inputArray[i];
+        secondItem = inputArray[(i + 1)];
+        pairArray = [firstItem, secondItem]
+      } else {
+        firstItem = inputArray[i];
+        pairArray = [firstItem]
+      }
+      
+      row.push(pairArray);
+      outputArray.push(row);
+    }
+
+    return outputArray;
   }
 }
 
