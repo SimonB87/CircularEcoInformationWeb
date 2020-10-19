7bc7350e85d36002ffcf29883d7a1309a8b1df54
SimonB87 <buryans@gmail.com>
2020-10-19 18:32:52 +0200

    mPDF - added extra config for right table

---
+++ sollution/config/config3.php
@@ -0,0 +1,20 @@
+<?php
+
+ob_start(); //Turns on output buffering
+
+session_start();
+
+$timezone = date_default_timezone_set("Europe/London");
+
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="cirkprojekty";
+// Create the connection
+$con= new mysqli($servername,$username,$password,$dbname);
+//Check Connection
+if($con->connect_error) {
+      die("Connection failed: " . $con->connect_error);
+}
+
+?>
