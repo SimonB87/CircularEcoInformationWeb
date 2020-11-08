9f93d38022b6d8d87322437442e948c591f4eb15
SimonB87 <buryans@gmail.com>
2020-11-08 16:57:32 +0100

    designTemplate2020 - menu with icons - sollution site

--- sollution/includes/tableprojectswebmenu.php
+++ sollution/includes/tableprojectswebmenu.php
@@ -3,15 +3,32 @@
   <nav id="tableProjectsWeb_heading_navigation" class="topNavigationDesign2020_heading_navigation">
     <div class="topnav" id="myTopnav">
       <a href="http://obcevkruhu.cz/" target="_blank"><img src="assets/images/logoobcevkruhu_small_bw.png" alt="Obce v kruhu - logo" class="topnav_logo"></a>
-      <a href="allresults.php" class="active float-right_largeDisplay">Všechna Typová řešení</a>
-      <a href="http://obcevkruhu.cz/social/index.php" target="_blank" class="float-right_largeDisplay">Sociální síť</a>
+      <a href="allresults.php" class="active float-right_largeDisplay">
+        <div class="topnav__item">
+          <div class="topnav__item__icon"> <i class="fas fa-angle-down"></i> </div>
+          <div class="topnav__item__label"> Všechna Typová řešení </div>
+        </div>
+      </a>
+      <a href="http://obcevkruhu.cz/social/index.php" target="_blank" class="float-right_largeDisplay">
+        <div class="topnav__item">
+          <div class="topnav__item__icon"> <i class="fas fa-angle-down"></i> </div>
+          <div class="topnav__item__label"> Sociální síť </div>
+        </div>
+      </a>
       <div class="dropdown float-right_largeDisplay">
-        <button class="dropbtn">Kontakt 
-          <i class="fa fa-caret-down"></i>
+        <button class="dropbtn">
+          <div class="topnav__item__icon"> <i class="fas fa-angle-down"></i> </div>
+          <div class="topnav__item__label"> Kontakt <i class="fa fa-caret-down"></i> </div>
         </button>
         <div class="dropdown-content">
-          <a href="http://www.obcevkruhu.cz/social/contact.php" target="_blank">Správce aplikace</a>
-          <a href="http://obcevkruhu.cz/#contact" target="_blank">Projektový tým</a>
+          <a href="http://www.obcevkruhu.cz/social/contact.php" target="_blank">
+            <div class="topnav__item__icon"> <i class="fas fa-angle-down"></i> </div>
+            <div class="topnav__item__label"> Správce aplikace </div>
+          </a>
+          <a href="http://obcevkruhu.cz/#contact" target="_blank">
+            <div class="topnav__item__icon"> <i class="fas fa-angle-down"></i> </div>
+            <div class="topnav__item__label"> Projektový tým </div>
+          </a>
         </div>
       </div> 
       <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="responsiveDropDownMenu();">&#9776;</a>
