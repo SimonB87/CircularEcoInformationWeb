61123210770c07d9d13b2636b61d73785483dc63
SimonB87 <buryans@gmail.com>
2020-11-08 16:44:55 +0100

    designTemplate2020 - nav site HTML structure

--- sollution/includes/tableprojectswebmenu.php
+++ sollution/includes/tableprojectswebmenu.php
@@ -1,10 +1,23 @@
-<div id="tableProjectsWeb_heading" class="col-md-12 col-xs-12">
-  <nav id="tableProjectsWeb_heading_navigation">
-    <ul id="tableProjectsWeb_heading_navigationList">
-      <li class="navigationList_navItem"> <a href="http://obcevkruhu.cz/"> Domů </a> </li>
-      <li class="navigationList_navItem"> <a href="allresults.php"> Všechna Typová řešení </a></li>
-      <li class="navigationList_navItem"> <a href="http://obcevkruhu.cz/social/index.php" target="_blank"> Sociální síť </a> </li>
-      <li class="navigationList_navItem"> <a href="http://obcevkruhu.cz/#contact" target="_blank"> Kontakt </a> </li>
-    </ul>
+<div id="tableProjectsWeb_heading" class="topNavigationDesign2020_heading col-md-12 col-xs-12">
+
+  <nav id="tableProjectsWeb_heading_navigation" class="topNavigationDesign2020_heading_navigation">
+    <div class="topnav" id="myTopnav">
+      <a href="http://obcevkruhu.cz/" target="_blank"><img src="assets/images/logoobcevkruhu_small_bw.png" alt="Obce v kruhu - logo" class="topnav_logo"></a>
+      <a href="allresults.php" class="active float-right_largeDisplay">Všechna Typová řešení</a>
+      <a href="http://obcevkruhu.cz/social/index.php" target="_blank" class="float-right_largeDisplay">Sociální síť</a>
+      <div class="dropdown float-right_largeDisplay">
+        <button class="dropbtn">Kontakt 
+          <i class="fa fa-caret-down"></i>
+        </button>
+        <div class="dropdown-content">
+          <a href="http://www.obcevkruhu.cz/social/contact.php" target="_blank">Správce aplikace</a>
+          <a href="http://obcevkruhu.cz/#contact" target="_blank">Projektový tým</a>
+        </div>
+      </div> 
+      <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="responsiveDropDownMenu();">&#9776;</a>
+    </div>
   </nav>
+
 </div>
+
+<script src="assets/js/responsiveNavBar.js"></script>
